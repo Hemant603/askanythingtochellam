3e93d49370562ce09e7e4e3d92335b6e1d627c83
Hemant Panse <110870573+Hemant603@users.noreply.github.com>
2025-11-15 18:53:59 +0530

    Refactor Salesforce credentials access

--- netlify/functions/createAccount.js
+++ netlify/functions/createAccount.js
@@ -5,18 +5,18 @@ exports.handler = async (event, context) => {
     const { name, phone, website } = JSON.parse(event.body);
 
     // ---------- Salesforce Credentials ----------
-    const SF_LOGIN_URL = process.env.SF_LOGIN_URL;
-    const SF_USERNAME = process.env.SF_USERNAME;
-    const SF_PASSWORD = process.env.SF_PASSWORD;
-    const SF_TOKEN = process.env.SF_TOKEN;
+    const SF_LOGIN_URL =SF_LOGIN_URL;
+    const SF_USERNAME = SF_USERNAME;
+    const SF_PASSWORD = SF_PASSWORD;
+    const SF_TOKEN =SF_TOKEN;
 
     // ---------- Step 1: Login to Salesforce ----------
     const loginResponse = await axios.post(
       `${SF_LOGIN_URL}/services/oauth2/token`,
       new URLSearchParams({
         grant_type: "password",
-        client_id: process.env.SF_CLIENT_ID,
-        client_secret: process.env.SF_CLIENT_SECRET,
+        client_id: SF_CLIENT_ID,
+        client_secret: SF_CLIENT_SECRET,
         username: SF_USERNAME,
         password: SF_PASSWORD + SF_TOKEN,
       }),
